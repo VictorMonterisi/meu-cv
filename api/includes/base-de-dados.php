4013a88f931606e8a0410ba8767da652117f09c3
Victor Monterisi <victoraugustobm1@gmail.com>
2023-10-25 14:42:54 -0300

    Atualização Dados

--- api/includes/base-de-dados.php
+++ api/includes/base-de-dados.php
@@ -16,27 +16,11 @@
         "gitHub" => "https://github.com/VictorMonterisi",
         "codePen" => "https://codepen.io/victor-monterisi/pens/public", 
         "foto" => "assets/img/eu.jpg",
-        // "sobre" => "Redator / Copywriter e Desenvolvedor Web focado na criação de soluções que proporcionem valor tanto à empresa contratante, quanto aos clientes.",
         "sobre" => "Desenvolvedor Web focado na criação de soluções que proporcionem valor tanto à empresa contratante, quanto aos clientes.",
         "objetivo" => "Busco consolidar e aplicar meus conhecimentos para oferecer soluções excepcionais, proporcionando sistemas / sites altamente eficazes. Estou comprometido em contribuir significativamente, seja na criação de funcionalidades que atendam às necessidades do projeto ou no desenvolvimento de experiências online inovadoras."
     ];
 
     $experiencias_profissionais = [
-        // "cargo_4" => [
-        //     "nome" => "Líder de Desenvolvimento",
-        //     "tempo" => "11/2021 à 12/2022",
-        //     "descricao" => "Gestão da equipe de desenvolvedores e auxílio na elaboração de soluções visando proporcionar valor à empresa e aos clientes."
-        // ],
-        // "cargo_3" => [
-        //     "nome" => "Desenvolvedor Web",
-        //     "tempo" => "12/2018 à 12/2022",
-        //     "descricao" => "Desenvolvimento de websites com foco no posicionamento orgânico (seguindo as boas práticas da web e recomendações do W3C), edição de imagens, gerenciamento de campanhas SEO, elaboração de orçamentos referente ao serviço de posicionamento orgânico, apontamento de websites, suporte ao cliente nas questões de atualizações do site, recolhimento de feedback do cliente sobre os serviços prestados, manejamento de hospedagem e suporte de e-mails (criação e configuração)."
-        // ],
-        // "cargo_2" => [
-        //     "nome" => "Redator",
-        //     "tempo" => "07/2017 à 11/2018",
-        //     "descricao" => "Elaboração de conteúdo textual visando o posicionamento orgânico juntamente ao engajamento do público alvo."
-        // ],
         "cargo_2" => [
             "nome" => "Desenvolvedor Web",
             "tempo" => "07/2017 à 12/2022",
@@ -79,9 +63,7 @@
         "Git" => "git",
         "GitHub" => "github",
         "GitLab" => "gitlab",
-        // "Java" => "java",
         "JavaScript" => "js"
-        // "NodeJs" => "node",
     ];
 
     $tecnologias_extras = [
